3ea49310f7fb6ec9866e6f565258c9cd74df472c
Claraf05 <56109465+Claraf05@users.noreply.github.com>
2019-12-04 11:45:21 +0100

    Add files via upload

--- SpaceInvadersP2/SpaceInvadersP2/src/Commands/MoveCommand.java
+++ SpaceInvadersP2/SpaceInvadersP2/src/Commands/MoveCommand.java
@@ -24,9 +24,9 @@ public class MoveCommand extends Commands{
 	@Override
 	public Commands parse(String[] commandWord) throws MoveParseException {
 		Commands result = null;
-		if(commandWord[0].equals(name) || commandWord[0].equals(shortcut)) {
-			if(commandWord.length == 3 ) {
-				if(commandWord[1].equals("right") || commandWord[1].equals("right")) {
+		if(commandWord[0].equalsIgnoreCase(name) || commandWord[0].equalsIgnoreCase(shortcut)) {
+			if(commandWord.length > 3 ) {
+				if(commandWord[1].equals("right") || commandWord[1].equals("left")) {
 					dir = commandWord[1];
 					if(commandWord[2].equals("1") || commandWord[2].equals("2")) {
 						vel = Integer.parseInt(commandWord[2]);
