ba0a954b707396a0a0d3742028d9355f7bc71f0f
A.L.K <alejandrobarrachina.02@gmail.com>
2020-02-10 11:11:12 +0100

    Change things

--- Recuperación/src/interfaces/IComunications.java
+++ Recuperación/src/interfaces/IComunications.java
@@ -2,7 +2,7 @@ package interfaces;
 
 public interface IComunications {
 	default boolean isAlien() { //Returns if an object is alien or not
-		return true;
+		return true;//eliminar
 	}
 
 	default boolean isOnBorder(){//Only returns true if the object in the border is an alien ship
